e3a89e461e14bc47a3b216089a6cca6335d6619c
Bobby Iliev <bobby@bobbyiliev.com>
2023-01-31 20:23:42 +0200

    Fix UNION types integer and text cannot be matched

--- tests/materialize_dbt_test/models/example/my_first_dbt_model.sql
+++ tests/materialize_dbt_test/models/example/my_first_dbt_model.sql
@@ -13,15 +13,9 @@ with source_data as (
 
     select 1 as id
     union all
-    select null as id
+    select 2
 
 )
 
 select *
 from source_data
-
-/*
-    Uncomment the line below to remove records with null `id` values
-*/
-
--- where id is not null
